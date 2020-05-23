a6969f3ed3ad8df2065cb91a5289deb26bb48410
prabhucse17 <fH$!e44^>
2020-05-23 22:28:57 +0530

    Intial Development To login Screen

--- Login_Screen.py
+++ Login_Screen.py
@@ -1,3 +1,4 @@
 # Use this file for the development
 print("Enter Username")
 print("Enter Password")
+print("Click on Login Button use the code in Login.py")
---
+++ login.py
@@ -0,0 +1,2 @@
+print("1.get the driver ")
+print("install browser and then open page to login")
